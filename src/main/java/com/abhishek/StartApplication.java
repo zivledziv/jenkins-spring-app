faf5d128f4035b0b2b2ef0f634e5944d3caa58e8
zivledziv <zivledziv@gmail.com>
2024-01-17 13:41:49 +0200

    testing webhook

--- src/main/java/com/abhishek/StartApplication.java
+++ src/main/java/com/abhishek/StartApplication.java
@@ -12,7 +12,7 @@ public class StartApplication {
 
     @GetMapping("/")
     public String index(final Model model) {
-        model.addAttribute("title", "Hello");
+        model.addAttribute("title", "Helooo");
         model.addAttribute("msg", "This application is deployed on to Kubernetes using Argo CD");
         return "index";
     }
