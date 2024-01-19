68ebca0b52430ec8f143602fcfaf1edba053990a
zivledziv <zivledziv@gmail.com>
2024-01-19 12:45:03 +0200

    done

--- src/main/java/com/abhishek/StartApplication.java
+++ src/main/java/com/abhishek/StartApplication.java
@@ -12,7 +12,7 @@ public class StartApplication {
 
     @GetMapping("/")
     public String index(final Model model) {
-        model.addAttribute("title", "Hello there :)");
+        model.addAttribute("title", "Welcome :)");
         model.addAttribute("msg", "This application is deployed on to Kubernetes using Argo CD");
         return "index";
     }
